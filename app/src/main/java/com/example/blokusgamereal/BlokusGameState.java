35e55bb2672aa4c1cb0ba04b2086c8ab8b840cde
Max Clark <clarkm25@up.edu>
2022-02-24 15:45:29 -0800

    Added action methods.

--- app/src/main/java/com/example/blokusgamereal/BlokusGameState.java
+++ app/src/main/java/com/example/blokusgamereal/BlokusGameState.java
@@ -1,9 +1,10 @@
 package com.example.blokusgamereal;
 
 public class BlokusGameState {
-    private int playerScore;
-    private int playerTurn;
+    private int[] playerScore;
+    private int[] playerTurn;
     private boolean legalMove;
+    private boolean gameOn;
 
     //Default Constructor
     public BlokusGameState() {
@@ -13,5 +14,43 @@ public class BlokusGameState {
     //Deep Copy Constructor
     public BlokusGameState(int PLACEHOLDER) {
 
+    }
+
+    public boolean quitGame(boolean initGameOn) {
+        gameOn = initGameOn;
+        if (gameOn = false) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean placePiece() {
+        if (legalMove == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String gameStateInfo;
+
+        gameStateInfo = "" + gameOn + "%n";
+
+        gameStateInfo += "" + legalMove + "%n";
+
+        for (int i = 0; i < playerScore.length; i++) {
+            gameStateInfo += "Player " + playerScore[i] + "%n";
+        }
+
+        for (int i = 0; i < playerTurn.length; i++) {
+            gameStateInfo += "Player " + playerTurn[i] + "'s turn. %n";
+        }
+
+        return gameStateInfo;
     }
 }

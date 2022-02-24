1387f15a6a2db3841147bc87e7d9168fe1d041cd
skyelarCann <cann24@up.edu>
2022-02-24 15:37:41 -0800

    Added BlokusBlock Class and Changed Ctors

---
+++ app/src/main/java/com/example/blokusgamereal/BlokusBlock.java
@@ -0,0 +1,18 @@
+package com.example.blokusgamereal;
+
+public class BlokusBlock {
+    private int color;
+
+    /* No param ctor */
+    public BlokusBlock()
+    {
+        this.color = -1;
+    }
+
+
+    /* Setters of BlokusBlock variables */
+    public void setColor(int toSet)
+    {
+        this.color = toSet;
+    }
+}
